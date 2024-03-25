e9e7d7f94b95812c76d14a7f568c03d520130342
lilykarmila10 <lilykarmila10@gmail.com>
2024-03-25 17:04:49 +0800

    next commit

--- lib/view/screens/sub_category/sub_categories.dart
+++ lib/view/screens/sub_category/sub_categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ta_ecommerce/common/widgets/images/t_rounded_image.dart';
 import 'package:ta_ecommerce/common/widgets/products/product_card_horizontal.dart';
 import 'package:ta_ecommerce/common/widgets/text/section_heading.dart';
@@ -6,12 +7,17 @@ import 'package:ta_ecommerce/utils/constans/image_strings.dart';
 import 'package:ta_ecommerce/utils/constans/sizes.dart';
 
 import '../../../common/widgets/appbar/appbar.dart';
+import '../../../common/widgets/layouts/grid_layout.dart';
+import '../../../common/widgets/loaders/vertical_product_shimmer.dart';
+import '../../../common/widgets/products/product_card_vertical.dart';
+import '../../../controller/product_controller/product_controller.dart';
 
 class SubCategoriesScreen extends StatelessWidget {
   const SubCategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(ProductController());
     return Scaffold(
       appBar: TAppBar(title: Text('Kuliner'), showBackArrow: true),
       body: SingleChildScrollView(
@@ -19,10 +25,6 @@ class SubCategoriesScreen extends StatelessWidget {
           padding: EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
-              /// banner
-              TRoundedImage(width: double.infinity, imageUrl: TImages.banner2, applyImageRadius: true),
-              SizedBox(height: TSizes.spaceBtwSections),
-
               /// sub categories
               Column(
                 children: [
@@ -32,38 +34,20 @@ class SubCategoriesScreen extends StatelessWidget {
 
                   /// product card
 
-                  SizedBox(
-                    height: 120,
-                    child: ListView.separated(
-                      itemCount: 4,
-                      scrollDirection: Axis.horizontal,
-                      separatorBuilder: (context, index) => SizedBox(width: TSizes.spaceBtwItem),
-                      itemBuilder: (context, index) => TProductCardHorizontal(),
-                    ),
-                  ),
+                  Obx(() {
+                    if (controller.isLoading.value) return TVerticalProductShimmer();
+                    if (controller.featuredProducts.isEmpty) {
+                      return Center(
+                          child: Text('Data tidak ditemukan!', style: Theme.of(context).textTheme.bodyMedium));
+                    }
+                    return TGridLayout(
+                      itemCount: controller.featuredProducts.length,
+                      itemBuilder: (_, index) => TProductCardVertical(product: controller.featuredProducts[index]),
+                    );
+                  })
                 ],
               ),
               SizedBox(height: TSizes.spaceBtwItem),
-
-              Column(
-                children: [
-                  /// heading
-                  TSectionHeading(title: 'Kuliner daerah', onPressed: () {}),
-                  SizedBox(height: TSizes.spaceBtwItem / 2),
-
-                  /// product card
-
-                  SizedBox(
-                    height: 120,
-                    child: ListView.separated(
-                      itemCount: 4,
-                      scrollDirection: Axis.horizontal,
-                      separatorBuilder: (context, index) => SizedBox(width: TSizes.spaceBtwItem),
-                      itemBuilder: (context, index) => TProductCardHorizontal(),
-                    ),
-                  ),
-                ],
-              )
             ],
           ),
         ),
